62c4e293d61b459c9cafb005142b9d2f218c9f9c
Mitchell Browne <>
2022-02-21 16:30:56 +1100

    Parse cli options

--- src/checker.js
+++ src/checker.js
@@ -1,3 +1,24 @@
-export function envChecker(args) {
-  console.log(args);
-}
+import arg from 'arg';
+
+const parseArgumentsIntoOptions = rawArgs => {
+  const args = arg(
+    {
+      '--artifact': String,
+      '--maxEnvSize': Number,
+      '-a': '--artifact',
+      '-m': '--maxEnvSize',
+    },
+    {
+      argv: rawArgs.slice(2),
+    }
+  );
+  return {
+    artifact: args['--artifact'] || './.serverless',
+    maxEnvSize: args['--maxEnvSize'] || 4000,
+  };
+};
+
+export const envChecker = args => {
+  const options = parseArgumentsIntoOptions(args);
+  console.log(options);
+};
